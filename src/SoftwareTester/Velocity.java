ca12a69fc578dfa93f055bc02c188fd6a7a6902f
DELL <DELL@DESKTOP-TD77G35>
2022-10-13 17:24:05 +0530

    Sajiya first repo

---
+++ src/SoftwareTester/Velocity.java
@@ -0,0 +1,20 @@
+package SoftwareTester;
+
+public class Velocity 
+{
+public static void main(String[] args)
+{
+
+	int a =100;
+	int b= 200;
+	int c=300;
+	
+	int d = a+b+c;
+	System.out.println(d);
+	
+	
+	
+	
+	
+}
+}
